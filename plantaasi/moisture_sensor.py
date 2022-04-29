4c74f554381226a06f5d6aa9bcd11d34b34a9e7c
Stefan <stefan@neuland.io>
2022-04-29 23:53:10 +0200

    add initial code for moisture sensor

---
+++ plantaasi/moisture_sensor.py
@@ -0,0 +1,29 @@
+from machine import ADC
+
+
+class MoistureSensor:
+    READ_CYCLE = 8
+    MAX_REF = 32000
+
+    def __init__(self, pin, raw_high, raw_low):
+        # raw value to percental value mapping is calculated by using a linear
+        # funktion. Instead of processing the whole mapping, we can pre-process
+        # the params of the linear function instead (y = mx + n)
+        self.m = MoistureSensor.MAX_REF / (raw_high - raw_low)
+        self.n = (raw_low * MoistureSensor.MAX_REF) // (raw_low - raw_high)
+
+        self.adc = ADC(pin)
+        self.adc.atten(ADC.ATTN_11DB)
+        self.adc.width(ADC.WIDTH_10BIT)
+
+    def read_raw_u16(self):
+        raw = 0
+        for _ in range(MoistureSensor.READ_CYCLE):
+            raw += self.adc.read_u16()
+
+        return raw // MoistureSensor.READ_CYCLE
+
+    def read(self):
+        perc = (self.m * self.read_raw_u16() + self.n) / MoistureSensor.MAX_REF
+
+        return int(perc * 100)
