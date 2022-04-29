471078918b7a31eaa37be987644e570c45a8bc04
Stefan <stefan@neuland.io>
2022-04-30 00:42:51 +0200

    wire up a simple test setup

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import time
 
+from machine import Pin, Timer
+
+from plantaasi.grafana import Metric, Grafana
+from plantaasi.moisture_sensor import MoistureSensor
+
 from config import config
-from machine import Timer
 
 
 def init_wifi(essid, password):
@@ -21,32 +25,20 @@ def init_time():
     ntptime.settime()
 
 
-def rand_metric(url, instance_id, api_key):
-    import urequests as requests
-    import random
-
-    response = requests.post(
-        url,
-        headers={"Authorization": f"Bearer {instance_id}:{api_key}"},
-        json=[{'name': 'test.esp32',
-               'value': random.uniform(0.0, 100.0),
-               'interval': 60,
-               'time': time.time() + 946684800}]
-    )
-    response.close()
-
-
 def run():
     init_wifi(config['wifi']['essid'], config['wifi']['password'])
     init_time()
 
-    Timer(0).init(
-        period=60000,
-        mode=Timer.PERIODIC,
-        callback=lambda t: rand_metric(config['grafana']['metrics_url'],
-                                       config['grafana']['user'],
-                                       config['grafana']['api_key'])
-    )
+    metric = Metric('test.moisture')
+
+    grafana = Grafana(config['grafana']['metrics_url'],
+                      config['grafana']['user'],
+                      config['grafana']['api_key'])
+
+    sensor = MoistureSensor(Pin(34), 17320, 41728)
+
+    Timer(0).init(period=60000, mode=Timer.PERIODIC,
+                  callback=lambda t: grafana.push([metric(sensor.read())]))
 
 
 if __name__ == '__main__':
