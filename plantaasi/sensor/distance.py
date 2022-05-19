ac9c7ff84dffd2d5818346afa0899e0685540ac4
Stefan Walluhn <stefan@walluhn.de>
2022-05-19 22:50:37 +0200

    add SR04T distance sensor

---
+++ plantaasi/sensor/distance.py
@@ -0,0 +1,24 @@
+import time
+from machine import Pin, time_pulse_us
+
+
+class SR04T:
+    SONIC_SPEED = 343.2
+
+    def __init__(self, trigger_pin, echo_pin):
+        self.trigger_pin = trigger_pin
+        self.echo_pin = echo_pin
+
+    def _trigger(self):
+        self.trigger_pin.on()
+        time.sleep_us(10)
+        self.trigger_pin.off()
+
+    def _read_raw(self):
+        self._trigger()
+
+        return time_pulse_us(self.echo_pin, 1)
+
+    def read(self):
+        # raw / 1000000 * sonic speed / 2
+        return self._read_raw() * SR04T.SONIC_SPEED / 2000000
