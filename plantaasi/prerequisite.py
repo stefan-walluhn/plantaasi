bd733b3191ac5416f1a773ef6b4d7a5ba8163bd3
Stefan <stefan@neuland.io>
2022-07-07 23:12:30 +0200

    add prerequisites to check before watering

---
+++ plantaasi/prerequisite.py
@@ -0,0 +1,37 @@
+import time
+
+
+class Prerequisite:
+    def fulfilled(self):
+        raise NotImplementedError
+
+
+class Prerequisites(Prerequisite):
+    def __init__(self, *prerequisites):
+        self.prerequisites = prerequisites
+
+    def _each_fulfilled(self):
+        for prerequisite in self.prerequisites:
+            yield prerequisite.fulfilled()
+
+    def fulfilled(self):
+        return False not in self._each_fulfilled()
+
+        # Note to self: if we must cycle ALL fulfilled checks, we have to use
+        # `list` to empty the generator! Otherwise this will return on first
+        # hit:
+        # return False not in list(self._each_fulfilled())
+
+
+class MoisturePrerequisite(Prerequisite):
+    def __init__(self, sensor, threshold):
+        self.sensor = sensor
+        self.threshold = threshold
+
+    def fulfilled(self):
+        return self.threshold < self.sensor.read()
+
+
+class TimePrerequisite(Prerequisite):
+    def fulfilled(self):
+        return time.localtime()[3] not in range(6, 20)
