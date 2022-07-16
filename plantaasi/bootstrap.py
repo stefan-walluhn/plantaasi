f412b0713c0896b207488c00e048e00c4342f893
Stefan <stefan@neuland.io>
2022-07-16 15:15:06 +0200

    refactoring, use generic builders

--- plantaasi/bootstrap.py
+++ plantaasi/bootstrap.py
@@ -12,7 +12,7 @@ from plantaasi.watering import Watering
 
 
 class MoisturePrerequisiteBuilder:
-    def get_prerequisite(self, pin, raw_hight, raw_low, threshold):
+    def __call__(self, pin, raw_hight, raw_low, threshold):
         return MoisturePrerequisite(
             MoistureSensor(Pin(pin), raw_hight, raw_low),
             threshold=threshold
@@ -20,40 +20,34 @@ class MoisturePrerequisiteBuilder:
 
 
 class TimePrerequisiteBuilder:
-    def get_prerequisite(self):
+    def __call__(self):
         return TimePrerequisite()
 
 
 class PumpTriggerBuilder:
-    def get_trigger(self, pin, duration):
-        return PumpTrigger(Pump(Pin(pin, mode=Pin.OPEN_DRAIN, value=0)),
-                           duration)
+    def __call__(self, pin, duration):
+        return PumpTrigger(
+            Pump(Pin(pin, mode=Pin.OPEN_DRAIN, value=0)), duration
+        )
 
 
 class WateringBuilder:
     def __init__(self):
-        self._prerequisite_builder = {
-            'moisture': MoisturePrerequisiteBuilder(),
-            'time': TimePrerequisiteBuilder()
-        }
-
-        self._trigger_builder = {'pump': PumpTriggerBuilder()}
+        self._builder = {'moisture': MoisturePrerequisiteBuilder(),
+                         'time': TimePrerequisiteBuilder(),
+                         'pump': PumpTriggerBuilder()}
 
     def get_wattering(self, watering_config):
         return Watering(
             Prerequisites(
-                *self._prerequisites(watering_config['prerequisites'])
+                *self._build_members(watering_config['prerequisites'])
             ),
-            Triggers(*self._triggers(watering_config['triggers']))
+            Triggers(*self._build_members(watering_config['triggers']))
         )
 
-    def _prerequisites(self, prerequisites_config):
-        for k, v in prerequisites_config.items():
-            yield self._prerequisite_builder[k].get_prerequisite(**v)
-
-    def _triggers(self, triggers_config):
-        for k, v in triggers_config.items():
-            yield self._trigger_builder[k].get_trigger(**v)
+    def _build_members(self, config):
+        for k, v in config.items():
+            yield self._builder[k](**v)
 
 
 def init_wifi(essid, password):
@@ -76,3 +70,10 @@ def init_time():
             return
         except OSError:
             time.sleep(1)
+
+
+def init_waterings(waterings_config):
+    watering_builder = WateringBuilder()
+
+    for watering_config in waterings_config:
+        yield watering_builder.get_wattering(watering_config)
